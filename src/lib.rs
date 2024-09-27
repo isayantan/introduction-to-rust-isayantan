f5be6cf5768abc97270ef2f713894e113cc4a1b0
isayantan <sayantan.choudhury4@gmail.com>
2024-09-26 20:17:29 -0400

    Updated

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,13 @@
 pub mod sampler;
+
+use wasm_bindgen::prelude::*;
+
+[wasm_bindgen]
+pub fn run(){
+    const N_STEP: usize = 100:
+    const N_DIM: usize = 2;
+    let mut state = sample::State::<N_DIM>::new(0xdeadbeef);
+    for _ in 0..N_STEP {
+        state.take_step();
+        log(&format!("{:?}, {:?}", state.arr[0], state.arr[[1])
+}
---
+++ src/test.rs
@@ -0,0 +1,4 @@
+fn miain(){
+    let x: i32 = 5;
+    println!("The value of x is : {}", x);
+}
